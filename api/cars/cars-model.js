d2bc98e7f3f2a501160a8adef4edd52ee8a74dfc
thecuriousones <tamaraleonard46@gmail.com>
2021-08-22 16:37:04 -0400

    model created

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,20 @@
+const db = require('../../data/db-config.js')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('cars')
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = (id) => {
+  return db('cars')
+    .where({ id })
+    .first()
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+const create = (car) => {
+  const [id] = await db('cars').insert(car)
+
+  return getById(id)
 }
+
+
+module.exports = { getAll, getById, create }
